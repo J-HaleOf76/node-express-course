5b2bb7cb973b473384b113f339f48ef70d7bf061
J-HaleOf76 <johnhale1976@live.com>
2020-07-18 14:50:28 -0400

    add dynamic GET route

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const app = express();
 
 const mockUserData=[
     {name:'Mark'},
-    {name:'Jill'}
+    {name:'Jill'},
+    {name:'Amy'},
+    {name:'John'}
+
     ]
     app.get('/users', function(req,res){
          res.json({
@@ -16,5 +19,13 @@ const mockUserData=[
 
 
 app.listen(8000, function() {
+    app.get('/users/:id',function(req,res){
+        console.log(req.params.id)
+        res.json({
+            success: true,
+            message: 'got one user',
+            user: req.params.id
+        })
+    })
     console.log("Server Is Runnin From YOU!");
   })
